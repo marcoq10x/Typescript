d8c1d57e872da69cba32866972f08c7a04131b2f
marcoq10x <marcoq10x@gmail.com>
2023-12-17 02:43:27 -0500

    review

--- dist/model/note.js
+++ dist/model/note.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Note = void 0;
 class Note {
-    constructor(id, title, content, category, // Ensure this line is present
+    constructor(id, title, content, category, // this line must be present
     createdAt = new Date()) {
         this.id = id;
         this.title = title;
